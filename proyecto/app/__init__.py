63bfd935f2efd4f3c4c31bd560eb3a2185210174
Unknown <ismaelssj120@gmail.com>
2017-12-04 01:35:21 -0500

    actualizacion

--- proyecto/app/__init__.py
+++ proyecto/app/__init__.py
@@ -2,25 +2,9 @@ from flask import Flask, render_template,redirect, request
 import http.client
 import json,requests
 
-#print(response)
 app = Flask('__name__')
 
-#
-# connection = http.client.HTTPConnection('api.football-data.org')
-# headers = {'X-Auth-Token': '6f89e625709d4b11af33273c70007ae9', 'X-Response-Control': 'minified'}
-# connection.request('GET', '/v1/competitions/445/leagueTable', None, headers)
-# response = json.loads(connection.getresponse().read().decode())
-#
-# hola = response['standing']
-#
-# try:
-#     print(response)
-#     print(response['standing'])
-#     for x in response['standing']:
-#         print(x['team'], x['playedGames'], x['points'])
-# except Exception as e:
-#     print(e)
-#Cuando los datos no son tipo POST no los declares aqui arriba.
+
 
 
 @app.route('/',methods =['GET'])
@@ -34,6 +18,14 @@ def init():
     connection.request('GET', '/v1/competitions/445/leagueTable', None, headers)
     premier = json.loads(connection.getresponse().read().decode())
 
+    connection.request('GET', '/v1/competitions/455/leagueTable', None, headers)
+    bbva = json.loads(connection.getresponse().read().decode())
+    connection.request('GET', '/v1/competitions/452/leagueTable', None, headers)
+    bundes = json.loads(connection.getresponse().read().decode())
+    connection.request('GET', '/v1/competitions/456/leagueTable', None, headers)
+    serieA = json.loads(connection.getresponse().read().decode())
+    connection.request('GET', '/v1/competitions/450/leagueTable', None, headers)
+    ligue = json.loads(connection.getresponse().read().decode())
     #Es un poco importante detallas que tipo de llamado haces si es GET o POST asi le dices al servidor que camino escojer.
     if request.method == 'GET':
         #La excepcion es por si da el caso que revienta el codigo no te tire esa corretiada de cosas rojas
@@ -43,21 +35,14 @@ def init():
             #En este estas mandado todo el json al html para despues recorerlo en el mismo html
             #es mas facil y conveniente hacerlo asi ya que imprimes todos los valos de un tiro
             #y no tienes que hacer un segundo llamado para imprimir otro valor.
-            return render_template('init.html',response = response, premier = premier['standing'])
+            return render_template('init.html',response = response, premier = premier['standing'],bbva = bbva['standing'],
+                                   bundes = bundes['standing'], serieA = serieA['standing'],ligue = ligue['standing'])
             #Metodo 2
             #De esta forma estas mandando valor por valor cada ves que recargas la pagina imagina
             #que tengas 50 datos en el json tienes que recargar 50 veces para que aparescan no es malo
             #pero es muy lento...
-            # for x in response:
-            #     id = x['id']
-            #     copa = x['caption']
-            #     liga = x['league']
-            #
-            #     print(id,'', copa,'',liga)
-            #     # print('id',x["id"],'copa',x["caption"],'liga',x["league"],'año',x["year"],'dia_actual',x["currentMatchday"],'jornadas',x["numberOfMatchdays"],
-            #     #     'cantidad de equipos',  x["numberOfTeams"],
-            #     #   'numero de partidos',x["numberOfGames"],'actualizacion', x["lastUpdated"])
-            #     return render_template('init.html', id=id, copa=copa, liga=liga)
+
+
         except Exception as e:
             print(e)
     #Cuando estas utilizando un if por regla van dos return el que cumple la condicion de if y el que no
@@ -65,33 +50,9 @@ def init():
     #El que no los dejas vasio como el de abajo
     return render_template('init.html')
 
-<<<<<<< Updated upstream
-=======
-        #return render_template('inicio.html')
 
-@app.route('/premier',methods =['GET'])
-def premier():
-    connection = http.client.HTTPConnection('api.football-data.org')
-    headers = {'X-Auth-Token': '6f89e625709d4b11af33273c70007ae9 ', 'X-Response-Control': 'minified'}
-    connection.request('GET', '/v1/competitions/455/leagueTable', None, headers)
-    # connection.request('GET', '/v1/teams/524', None, headers )
-    response = json.loads(connection.getresponse().read().decode())
+    
 
-
-
-    if request.method == 'GET':
-      try:
-       print(response['standing'])
-       for x in response['standing']:
-        print(x['rank'], x['team'], x['playedGames'], x['points'], x['goals'], x['goalsAgainst'],
-                    x['goalDifference'])
-
-        return render_template('premier.html',response = response)
-      except Exception as e:
-             print(e)
-
-    return render_template('premier.html')
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app.run()
--- proyecto/app/ejemplo.py
+++ proyecto/app/ejemplo.py
@@ -4,21 +4,17 @@ from flask import Flask, render_template, request, redirect
 
 connection = http.client.HTTPConnection('api.football-data.org')
 headers = { 'X-Auth-Token': '6f89e625709d4b11af33273c70007ae9 ','X-Response-Control': 'minified' }
-connection.request('GET', '/v1/competitions/455/leagueTable', None, headers )
-#connection.request('GET', '/v1/teams/524', None, headers )
+connection.request('GET', '/v1/competitions/455/fixtures', None, headers )
+
 response= json.loads(connection.getresponse().read().decode())
 
-#print(response)
-#for x in response:
- #   print (x["id"])
-#for x in response:
-print(response['standing'])
-for n in response['standing']:
-    print(n['rank'],n['team'],n['playedGames'],n['points'],n['goals'],n['goalsAgainst'],n['goalDifference'],
-          )
+print(response)
 
-#print(x["rank"],x["team"],x["teamId"],x["playedGames"],
- #         x["points"],x["goal"],x["goalsAgainst"],x["goalDifference"])
+for x in response['fixtures']:
+
+    print(x['id'],x['competitionId'],x['date'],x['status'],x['matchday'],x['homeTeamName'],x['awayTeamName'])
+         # x['goalsHomeTeam'],x['goalsAwayTeam'])
+
 
 
 
